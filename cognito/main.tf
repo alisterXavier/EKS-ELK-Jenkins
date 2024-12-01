24c0c08b90c22fef7104680021ae9d6ae010ad1e
Alister Xavier <xavieralister153@gmail.com>
2024-12-01 16:49:44 +0000

    'aws'

--- cognito/main.tf
+++ cognito/main.tf
@@ -41,7 +41,7 @@ resource "aws_cognito_identity_pool" "identity_pool" {
   allow_unauthenticated_identities = false
   cognito_identity_providers {
     client_id               = aws_cognito_user_pool_client.pool.id
-    provider_name           = "cognito-idp.${data.aws_region.current.name}.amazonaws.com/${aws_cognito_user_pool.user-pool.id}"
+    provider_name           = "cognito-idp.${var.region}.amazonaws.com/${aws_cognito_user_pool.user-pool.id}"
     server_side_token_check = false
   }
 }
